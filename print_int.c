c5ee2572ad2bce661fe92431b0a35683a30c7a5c
Gachenge <gachenge1@gmail.com>
2022-10-19 11:50:38 +0000

    string and numbers

--- print_int.c
+++ print_int.c
@@ -3,31 +3,41 @@
 /**
  * print_int - print integers
  * @ptr: variadic
- * Return: count
+ * Return: 0
  */
 
 int print_int(va_list ptr)
 {
-	int i, num, count, arr[10];
+	int arr[10];
+	int i, num, count, large, sum;
 	char x[1];
 
 	num = va_arg(ptr, int);
-
-	while (num)
+	count = 0;
+	large = 1000000000;
+	arr[0] = num / large;
+	
+	for (i = 1; i <10; i++)
 	{
-		arr[i] = num % 10;
-		num = num / 10;
-		if (num == 0)
-			break;
-		else
-			i++;
+		large = large / 10;
+		arr[i] = (num / large) % 10;
 	}
-	while (i >= 0)
+	if (num < 0)
 	{
-		x[0] = ('0' + arr[i]);
-		write(1, x, 1);
-		count++;
-		i--;
+		write(1, "-", 1);
+		count ++;
+		for (i = 0; i < 10; i++)
+			arr[i] *= -1;
+	}
+	for (i = 0, sum = 0; i < 10; i++)
+	{
+		sum += arr[i];
+		if (sum != 0 || i == 9)
+		{
+			x[0] = ('0' + arr[i]);
+			write (1, x, 1);
+			count++;
+		}
 	}
 	return (count);
 }
