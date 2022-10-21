dcc3f5b5c4b7bb938be33f8faf2da18ff229c593
Gachenge <gachenge1@gmail.com>
2022-10-21 12:05:09 +0000

    check octal

--- print_octal.c
+++ print_octal.c
@@ -6,13 +6,13 @@
  * Return: 0
  */
 
-int print_octal(va_list oct)
+int print_octal(va_list ptr)
 {
 	unsigned int i, num, max, arr[11];
-	int count = 0;
+	int count = 0, sum = 0;
 	char x[1];
 
-	num = va_arg(oct, unsigned int);
+	num = va_arg(ptr, unsigned int);
 	max = 1073741824;
 
 	arr[0] = num / max;
@@ -24,9 +24,13 @@ int print_octal(va_list oct)
 	}
 	for (i = 0; i < 11; i++)
 	{
+		sum += arr[i];
+		if (sum)
+		{
 			x[0] = ('0' + arr[i]);
 			write(1, x, 2);
 			count++;
+		}
 	}
 	return (count);
 }
