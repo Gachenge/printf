ebc93a017828548f4c3c15c557756f8fc393110b
Gachenge <gachenge1@gmail.com>
2022-10-21 16:29:43 +0000

    non-printable

--- print_S.c
+++ print_S.c
@@ -1,17 +1,41 @@
 #include "main.h"
 
 /**
+ * print_hX - convert to hex
+ * @c: the character
+ * Return: 0
+ */
+
+int print_hX(char c)
+{
+	int i;
+	char x[2];
+
+	x[0] = c / 16;
+	x[1] = c %16;
+
+	for (i = 0; i < 2; i++)
+	{
+		if (x[i] > 9)
+			_putchar('0' + 7 + x[i]);
+		else
+			_putchar('0' + x[i]);
+	}
+	return (i);
+}
+
+/**
  * print_S - print non printable characters
  * @str: variadic string
  * Return: 0
  */
 
-int print_S(va_list str)
+int print_S(va_list S)
 {
 	unsigned int i, count = 0;
 	char *x;
 
-	x = va_arg(str, char*);
+	x = va_arg(S, char *);
 	if (!x)
 		x = "(null)";
 	for(i = 0; x[i]; i++)
@@ -21,7 +45,7 @@ int print_S(va_list str)
 			_putchar('\\');
 			_putchar('x');
 			count += 2;
-			count += print_hex(x[i]);
+			count += print_hX(x[i]);
 		}
 		else
 		{
